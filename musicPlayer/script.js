48369844733af7380aee49e9cde5a89b3bdc5f64
playerking3 <murilomarquesdecastro@gmail.com>
2023-06-29 17:26:57 -0300

    troca de musica automática em progresso

--- musicPlayer/script.js
+++ musicPlayer/script.js
@@ -1,6 +1,7 @@
 let tabela = document.getElementById("tabela")
+files = ""
 function handleFileSelect(event) {
-    const files = event.target.files; // Obtém a lista de arquivos selecionados
+    files = event.target.files; // Obtém a lista de arquivos selecionados
 
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
@@ -10,21 +11,56 @@ function handleFileSelect(event) {
             const duration = audio.duration;	// Faça algo com a duração do arquivo de áudio aqui
             console.log(duration);
             tabela.innerHTML += '<tr>'
-            tabela.innerHTML += '<td onclick="tocar(this)">' + file.name + '</td> <td>' + parseInt(duration) + '</td>'
+            tabela.innerHTML += '<td onclick="tocar(this)">' + file.name + '</td> <td>' + formatDuration(duration) + '</td>'
             tabela.innerHTML += '</tr>'
 
         };
 
         audio.src = URL.createObjectURL(file); // Define o arquivo de áudio para o elemento <audio>
+
     }
 }
 
 function tocar(audio){
+
     let nome = audio.innerHTML
     let player = document.getElementById("audio")
     player.innerHTML = `
-    <audio controls autoplay>
+    <audio controls autoplay id='som'>
         <source src="playlist/`+nome+`" type="audio/wav" id="player">
     </audio>
     `
+    som.addEventListener("ended", () =>{
+        let index = null;
+        for (let i = 0; i < files.length; i++){
+            if (files[i].name == nome){
+                index = i;
+            }
+
+        }
+        if (index == files.length){
+            index = 0
+            player.innerHTML = `
+                <audio controls autoplay id='som'>
+                    <source src="playlist/`+files[index].name+`" type="audio/wav" id="player">
+                </audio>
+                `
+        }else{
+            index += 1
+            player.innerHTML = `
+                <audio controls autoplay id='som'>
+                    <source src="playlist/`+files[index].name+`" type="audio/wav" id="player">
+                </audio>
+                `
+        }
+    })
+    
 }
+
+
+function formatDuration(duration) {
+    const minutes = Math.floor(duration / 60);
+    const seconds = Math.floor(duration % 60);
+    const formattedDuration = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    return formattedDuration;
+  }
